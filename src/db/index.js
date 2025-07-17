52c29093de4c8056ee2bdadca17ab955f33a6bdd
bhavy1204 <bhavysarwa51@gmail.com>
2025-07-17 08:45:57 +0530

    connet to DB

---
+++ src/db/index.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+const connectDB = async () => {
+    try {
+        const connectionStatus = await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
+        console.log("Db connected. DB Host=> " + connectionStatus.connection.host);
+    } catch (error) {
+        console.log("MongoDB Connction error =>" + error);
+        process.exit(1);
+    }
+}
+export default connectDB;
