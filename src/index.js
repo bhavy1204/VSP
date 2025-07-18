3bf6e80567fa33cd025951da522b25196ef578a4
bhavy1204 <bhavysarwa51@gmail.com>
2025-07-18 22:18:49 +0530

    create API error and API response class

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
 import dotenv from "dotenv"
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({ path: "./env" })
 
-connectDB();
+connectDB()
+.then(()=>{
+    app.on("error",(err)=>{
+        console.log("Express error "+err);
+    })
+    app.listen(process.env.PORT || 3000,()=>{
+        console.log("server up and running");
+    })
+})
+.catch((err)=>{
+    console.log(err);
+});
