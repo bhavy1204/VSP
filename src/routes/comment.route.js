197797d9aaa93e6e00ffe29ed035063c149d2fb1
bhavy1204 <bhavysarwa51@gmail.com>
2025-08-23 09:11:43 +0530

    rename file

---
+++ src/routes/comment.route.js
@@ -0,0 +1,12 @@
+import { Router } from "express";
+import { addComment,deleteComment,updateComment,getVideoComment } from "../controllers/comment.controller.js";
+import {verifyJwt} from "../middlewares/auth.middleware.js";
+
+const router = Router();
+
+router.use(verifyJwt);
+
+router.route("/v/:videoId").get(getVideoComment).post(addComment);
+router.route("/c/:commentId").delete(deleteComment).patch(updateComment);
+
+export default router;
