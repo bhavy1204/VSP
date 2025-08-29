07344b8d81a2a309eebafd17c88fbcacd9ba97ce
bhavy1204 <bhavysarwa51@gmail.com>
2025-08-29 11:35:57 +0530

    chore: move file from user to componenets folder

---
+++ Frontend/src/components/Navbar.jsx
@@ -0,0 +1,47 @@
+import AddIcon from '@mui/icons-material/Add';
+import NotificationsIcon from '@mui/icons-material/Notifications';
+import PersonIcon from '@mui/icons-material/Person';
+import SearchIcon from '@mui/icons-material/Search';
+import MenuIcon from '@mui/icons-material/Menu';
+import YouTubeIcon from '@mui/icons-material/YouTube';
+
+
+export default function Navbar() {
+    return (
+        <>
+            <header className='flex justify-between px-2 py-2 border-gray-500 items-center bg-gray-900'>
+                <div className="static flex ml-3 gap-4 text-gray-300">
+                    <div className="menu">
+                        <button>
+                            <MenuIcon fontSize='large' />
+                        </button>
+                    </div>
+                    <div className="logo flex text-gray-300 items-center gap-4">
+                        <YouTubeIcon fontSize='large' /> <p> MultiVerseK</p>
+                    </div>
+                </div>
+                <div className="search border border-white rounded-xl w-1/2 px-2 py-0.5">
+                    <SearchIcon className='text-white' />
+                    <input type="text" className='w-[90%] focus:outline-none text-gray-300 ml-2' />
+                </div>
+                <div className="actions flex items-center text-white gap-4">
+                    <div className="create">
+                        <button className='flex items-center gap-2'>
+                            <AddIcon fontSize='medium' /><p>Create</p>
+                        </button>
+                    </div>
+                    <div className="notifications">
+                        <button onClick={console.log("ok")}>
+                            <NotificationsIcon fontSize='medium' />
+                        </button>
+                    </div>
+                    <div className="profike">
+                        <button>
+                            <PersonIcon fontSize='medium' />
+                        </button>
+                    </div>
+                </div>
+            </header>
+        </>
+    )
+}
