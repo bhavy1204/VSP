3e51982c78a6303666a6c825eabfab7fb41b4d27
bhavy1204 <bhavysarwa51@gmail.com>
2025-09-01 14:02:49 +0530

    implement videoPlaying component

---
+++ Frontend/src/components/Home/Commentscard.jsx
@@ -0,0 +1,18 @@
+import { ThumbsDown, ThumbsUp } from "lucide-react"
+
+export default function Commentscard(){
+    return(
+        <div className="p-5 flex flex-col gap-2 bg-gray-950 my-5 rounded-2xl">
+            <div className="user text-indigo-600">
+                UserName
+            </div>
+            <div className="comment text-gray-600">
+                Meow meow meow . Rasode me kon tha ?. 
+            </div>
+            <div className="likes flex text-white font-thin gap-6 size-14 items-center h-fit">
+                <ThumbsUp />
+                <ThumbsDown />
+            </div>
+        </div>
+    )
+}
--- Frontend/src/components/Home/Videocard.jsx
+++ Frontend/src/components/Home/Videocard.jsx
@@ -1,24 +1,26 @@
-export default function VideoCard(){
-    return(
+export default function VideoCard() {
+    return (
         <>
-            <div className="card-container m-4 bg-gray-900 rounded-2xl">
-                <div className="thumbnail flex justify-center pt-2">
-                    <img src="thumbnail.webp" alt="" className="rounded-sm" />
-                </div>
-                <div className="data">
-                    <div className="title text-white flex justify-start px-4 py-1">
-                        Sample title
+            <a href="/videoPlaying">
+                <div className="card-container m-4 bg-gray-900 rounded-2xl">
+                    <div className="thumbnail flex justify-center pt-2">
+                        <img src="thumbnail.webp" alt="" className="rounded-sm" />
                     </div>
-                    <div className="details flex justify-around px-2 py-2 text-gray-400">
-                        <div className="views">
-                            45k views 
+                    <div className="data">
+                        <div className="title text-white flex justify-start px-4 py-1">
+                            Sample title
                         </div>
-                        <div className="uploadedAT">
-                            8 months ago
+                        <div className="details flex justify-around px-2 py-2 text-gray-400">
+                            <div className="views">
+                                45k views
+                            </div>
+                            <div className="uploadedAT">
+                                8 months ago
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
+            </a>
         </>
     )
 }
